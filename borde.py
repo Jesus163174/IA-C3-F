7e3938943b15a038622e5a61ccec3da560f7195f
Jesus163174 <163174@ids.upchiapas.edu.mx>
2019-08-04 16:27:23 -0500

    stop images

--- borde.py
+++ borde.py
@@ -1,8 +1,9 @@
 import numpy as np
 import cv2
- 
+import tensorflow
+import keras
 #Cargar la mascara
-imagen = cv2.imread('m8.jpeg',0)
+imagen = cv2.imread('images/15.jpg',0)
  
 #Crear un kernel de '1' de 3x3
 kernel = np.ones((3,3),np.uint8)
@@ -14,3 +15,9 @@ transformacion = cv2.dilate(imagen,kernel,iterations = 1) - cv2.erode(imagen,ker
 cv2.imshow('resultado',transformacion)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
+print('tensorflow: %s' % tensorflow.__version__)
+# keras
+
+print('keras: %s' % keras.__version__)
